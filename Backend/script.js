6a838cabeccc74c5ff2bb7c6c68e263e9424adb4
Draweshwarkumar <shivamjha22948@gmail.com>
2024-07-20 20:39:37 +0530

    Initial commit

---
+++ Backend/script.js
@@ -0,0 +1,23 @@
+// let n = 5;
+//  for(let i = 0;i < n; i++){
+//     console.log("Hello" + i);
+//  }
+//  console.log("Bye");
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2;i< args.length;i++){
+//     console.log("hello to ",args[i]);
+// }
+
+// const math = require("./math");
+
+// console.log(math.sum(5,6));
+// console.log(math.mul(5,6));
+// console.log(math.pi);
+// console.log(math.g);
+
+const fruits = require("./fruits");
+console.log(fruits);
